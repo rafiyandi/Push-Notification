ac857d36ea075d9fe57a9ef05d1c67a7f8eadfa6
rafiyandi <rafiyandi1905@gmail.com>
2023-03-24 10:48:39 +0700

    configuration firebase with cli

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:pushnotification/app_widget.dart';
+import 'package:pushnotification/firebase_options.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const AppWidget());
 }
